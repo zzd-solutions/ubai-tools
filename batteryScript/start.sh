e38273fb683cefc331aef51707c2621111834043
zzd-solutions <140008024+zzd-solutions@users.noreply.github.com>
2025-09-17 17:38:14 +0300

    Update start.sh

--- batteryScript/start.sh
+++ batteryScript/start.sh
@@ -1,3 +1,16 @@
 #!/bin/bash
 #version-1-0-0
-echo "I ran"
+display_battery_info() {
+  echo "      IZOT BATTERY TOOL - powered by TLP      "
+  echo "----------------------------------------------"
+  sudo echo "$(tlp-stat -b"
+  echo "----------------------------------------------"
+  echo "Press Ctrl+C to exit!"
+  echo "After that use 1 and then press Enter to launch PCAuditor"
+}
+
+while true; do
+  clear
+  display_battery_info
+  sleep 1
+done
